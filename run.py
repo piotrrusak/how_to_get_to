8338d0732279128aaef9921dc449191d5f3b9818
Horacjusz <pawel.prus05@gmail.com>
2025-05-18 01:36:19 +0200

    Created full visualization for graph grid

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from utils.load_state_graph import load_state_graph
 from src.ant_colony import AntColony
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
-from data.data_1 import starting_times, lines
-from utils.path_utils import beautify_path
+from data.data_1 import starting_times, lines, colors
+from utils.path_utils import beautify_path, find_full_path
+from utils.visualize_graph import visualize_map
+from utils.printing import print_color
 
 if __name__ == "__main__":
 
@@ -11,17 +15,20 @@ if __name__ == "__main__":
     aco = AntColony(state_graph, len(lines), n_ants=5, n_iterations=200, alpha=1.0, beta=5.0, evaporation_rate=0.3, q=500)
 
     start_node = (0, 0)
-    end_node = (9, 9)
+    end_node = (9, 2)
     start_time = 0
 
     try:
+        # visualize_map(lines, colors)
         best_path, best_length = aco.run(start=start_node, end=end_node, start_time=start_time)
-        best_path = beautify_path(best_path)
+        final_path = beautify_path(best_path)
 
         print("\nBest path found:")
-        for point in best_path:
-            print(point)
+        for point in final_path:
+            print_color(point[:-1], color=colors[point[2]])
         print(f"Path length: {best_length} minutes")
-
+        
+        full_path = find_full_path(final_path, lines)
+        visualize_map(lines, colors, full_path)
     except ValueError as e:
         print(e)
--- utils/path_utils.py
+++ utils/path_utils.py
@@ -8,8 +8,8 @@ def beautify_path(path):
     for i in range(1, len(path) - 1) :
         if path[i-1][0] == path[i][0] == path[i+1][0] :
             continue
-        # if path[i-1][2] == path[i][2] == path[i+1][2] :
-        #     continue
+        if path[i-1][2] == path[i][2] == path[i+1][2] and path[i][0] != path[i-1][0] and path[i][0] != path[i+1][0] :
+            continue
         if path[i][0] == cleared_path[-1][0] and len(cleared_path) == 1 :
             cleared_path.pop()
 
@@ -17,3 +17,24 @@ def beautify_path(path):
     cleared_path.append(path[-1])
 
     return cleared_path
+
+def find_full_path(path, lines) :
+    full_path = []
+
+    path_parts = []
+
+    for i in range(0, len(path), 2) :
+        path_parts.append([path[i], path[i+1]])
+    for part in path_parts :
+        start_point, _, line = part[0]
+        end_point = part[1][0]
+        add = False
+        for point in lines[line] :
+            if point[0] == start_point :
+                add = True
+            if add :
+                full_path.append((point[0], line))
+            if point[0] == end_point :
+                break
+
+    return full_path
