420a579ea9adc6e868e7d6138fd5ec5384ef7e05
Antoni Kucharski <antek.kuchar@gmail.com>
2025-05-19 22:53:54 +0200

    feat: diagonals imputing

--- run.py
+++ run.py
@@ -7,9 +7,10 @@ from data.data_3 import starting_times, lines, colors
 from utils.path_utils import beautify_path, find_full_path
 from utils.visualize_graph import visualize_map
 from utils.printing import print_color
+from utils.impute import impute_lines
 
 if __name__ == "__main__":
-
+    lines = impute_lines(lines)
     state_graph = load_state_graph(lines, starting_times)
 
     aco = AntColony(state_graph,
@@ -21,12 +22,12 @@ if __name__ == "__main__":
                     evaporation_rate=0.3,
                     q=15)
 
-    start_node = (21, 61)
-    end_node = (95, 48)
+    start_node = (2, 50)
+    end_node = (90, 0)
     start_time = 60
 
     try:
-        # visualize_map(lines, colors)
+        visualize_map(lines, colors)
         best_path, best_length = aco.run(start=start_node, end=end_node, start_time=start_time)
         final_path = beautify_path(best_path)
 
