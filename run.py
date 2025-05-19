ead9c0a1d6d8ba84d8065cd43debed94b62c7b32
Antoni Kucharski <antek.kuchar@gmail.com>
2025-05-19 09:48:00 +0200

    feat: 9 more lines added

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from src.ant_colony import AntColony
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
-from data.data_1 import starting_times, lines, colors
+from data.data_3 import starting_times, lines, colors
 from utils.path_utils import beautify_path, find_full_path
 from utils.visualize_graph import visualize_map
 from utils.printing import print_color
@@ -12,10 +12,10 @@ if __name__ == "__main__":
 
     state_graph = load_state_graph(lines, starting_times)
 
-    aco = AntColony(state_graph, len(lines), n_ants=5, n_iterations=200, alpha=1.0, beta=5.0, evaporation_rate=0.3, q=500)
+    aco = AntColony(state_graph, len(lines), n_ants=10, n_iterations=200, alpha=1.0, beta=5.0, evaporation_rate=0.3, q=500)
 
-    start_node = (0, 0)
-    end_node = (9, 2)
+    start_node = (21, 61)
+    end_node = (30, 74)
     start_time = 0
 
     try:
